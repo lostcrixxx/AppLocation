a65ca5cd706a978aadc4358330fa75860800f49b
Cristiano Morais <desenvolvimento@amaral.anexxa>
2018-11-19 17:45:55 -0200

    Salvando no banco de dados

--- TiLocationMobile/app/src/main/java/br/com/tisoftware/tilocationmobile/MainActivity.java
+++ TiLocationMobile/app/src/main/java/br/com/tisoftware/tilocationmobile/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.tisoftware.tilocationmobile;
 
 import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -13,13 +14,33 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView t;
     private LocationManager locationManager;
     private LocationListener listener;
+    private String coordenadas;
+
+    // Banco de dados
+    Button register;
+    RequestQueue requestQueue;
+    String insertUrl = "http://tisoftware.atspace.cc/insert.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         t = (TextView) findViewById(R.id.textView);
+        coordenadas = t.getText().toString();
+
+        // Teste para inserir no banco
+        register = (Button) findViewById(R.id.register);
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
@@ -36,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
             public void onLocationChanged(Location location) {
                 t.append("\n " + location.getLongitude() + " " + location.getLatitude());
                 Log.i("localizacao","Enviado");
+                registrar();
+                Log.i("localizacao","Cadastrando");
             }
 
             @Override
@@ -56,8 +83,65 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-
         verificaGPS();
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registrar();
+            }
+        });
+
+        /*
+        // Registrar os dados no banco de dados
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //if(email.toString().equals(emailN.toString())) {
+                StringRequest request = new StringRequest(Request.Method.POST, insertUrl, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        System.out.println(response.toString());
+                        Log.i("localizacao", "cadastrando" + response.toString());
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> parameters = new HashMap<String, String>();
+                        parameters.put("nome", "teste1");
+                        parameters.put("licenca", "teste2");
+
+                        //parameters.put("nome", nome.getText().toString());
+                        //parameters.put("email", email.getText().toString());
+
+                        return parameters;
+                    }
+                };
+                requestQueue.add(request);
+
+
+                //} else{
+                //Toast.makeText(getApplicationContext(), "Por favor, verifique o email", Toast.LENGTH_SHORT).show();
+
+                //}
+
+
+            }
+
+        });
+
+        */
+
     }
 
     @Override
@@ -81,8 +165,43 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        locationManager.requestLocationUpdates("gps", 10000, 0, listener);
+        locationManager.requestLocationUpdates("gps", 15000, 0, listener);
     }
+
+
+    private void registrar() {
+        final ProgressDialog loading = ProgressDialog.show(this, "Por favor espere...", "Atualizando dados...", false, false);
+        String REGISTER_URL = "http://tisoftware.atspace.cc/insert.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        loading.dismiss();
+                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        loading.dismiss();
+                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("name", coordenadas);
+                //params.put("imei", "teste2");
+                params.put("licenca", "teste3");
+                Log.i("localizacao","Sucesso");
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
 }
 
 
