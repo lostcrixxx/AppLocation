6f15b060f55d5381ebf910994a0e2cc2000c99f3
Cristiano Morais <desenvolvimento@amaral.anexxa>
2018-11-20 16:40:31 -0200

    Pegando IMEI do aparelho

---
+++ TiLocationMobile/app/src/main/java/br/com/tisoftware/tilocationmobile/BroadCast.java
@@ -0,0 +1,12 @@
+package br.com.tisoftware.tilocationmobile;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BroadCast extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
