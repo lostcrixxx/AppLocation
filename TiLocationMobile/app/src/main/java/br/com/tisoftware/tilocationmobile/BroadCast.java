1961f44346a82bcb0689828311750798acb8490a
Cristiano Morais <desenvolvimento@amaral.anexxa>
2018-11-20 17:55:55 -0200

    Adicionando BroadcastReceiver

--- TiLocationMobile/app/src/main/java/br/com/tisoftware/tilocationmobile/BroadCast.java
+++ TiLocationMobile/app/src/main/java/br/com/tisoftware/tilocationmobile/BroadCast.java
@@ -5,8 +5,14 @@ import android.content.Context;
 import android.content.Intent;
 
 public class BroadCast extends BroadcastReceiver {
+
     @Override
     public void onReceive(Context context, Intent intent) {
+
+            if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+                Intent pushIntent = new Intent(context, Servico.class);
+                context.startService(pushIntent);
+            }
 
     }
 }
